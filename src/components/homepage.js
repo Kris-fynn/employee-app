09f18c0cb69fe93d8f05229c5827c202a58d94c7
Kris-fynn <67267786+Kris-fynn@users.noreply.github.com>
2022-06-20 17:03:05 +0200

    update

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,7 +1,24 @@
-
+import React,{useState} from 'react'
+import{Link} from 'react-router-dom'
 function Homepage() {
+   const [user,setUser]=useState(JSON.parse(localStorage.getItem("user")));
     return (
-           <div><h1>Employee Home employee profile and other employees </h1></div> 
+      <div className='home'>  <h1>Employee Details</h1>
+      <button ><Link  to="/">Log out</Link></button>
+      <div className='display'>
+      {user.map((user,index)=>{
+                          return(
+                              <div  className='displayinfo'>
+                                  <span> Name: <p>{user.name}</p> </span>
+                                  <span> Surname: <p>{user.surname}</p> </span>
+                                  <span> Email: <p>{user.email}</p></span>
+                                  <span> Contact number: <p>{user.contact}</p></span>
+                                  <span> Time: <p> {user.date} </p></span>
+                              </div>
+                          )
+                      })}
+      </div>
+      </div>
         );
      }
 export default Homepage;
--- src/components/signin.js
+++ src/components/signin.js
@@ -1,23 +1,68 @@
-
+import React,{useState} from 'react';
 import {Link} from 'react-router-dom'
-function Singin() {
+function Singin () {
+    const[ email, setEmail] =useState("");
+    const[ password, setPassword] =useState("");
+    const getData =(e)=>{
+      e.preventDefault();
+      // const getuserArr=JSON.stringify(localStorage.getItem("user"));
+      // console.log( getuserArr);
+      let logdetails={
+        email:email,
+        password:password
+      }
+      if(logdetails.email===""){
+        alert("Enter Your Email")
+      }else if(!logdetails.email.includes("@")){
+        alert("Your Email is missing an @  in it")
+      }else  if(logdetails.password===""){
+      alert("Enter Your Password")
+      }else if(logdetails.password.length<10){
+      alert("Your Password Must Have 10 Characrters")
+      }
+      else {
+        let storedUsers = JSON.parse(localStorage.getItem("user"));
+        let userFound=false;
+        for(let user of storedUsers) {
+          if(email === user.email && password === user.password){
+            alert("You have enter correct Email and password");
+            userFound=true
+            window.location = "/Homepage";
+          }
+         }
+         if(!userFound){
+          alert("You have enter wrong Email and password");
+         }
+      // if(email===getuserArr[email] && password===getuserArr[password]){
+      //   alert("You have enter coorect Email and password");
+      //   window.Location='/Home';
+      // }
+      // else if(getuserArr[email]==="" && getuserArr[password]===""){
+      //   alert("Your email or password emp");
+      // }
+      // else{
+      //   alert("Your email or password incorrect");
+      //   window.Location="/Home";
+      // }
+      }
     return (
            <div><h1>Employee Singin</h1>
               <form role="form" onsubmit="signIn(event);" autocomplete="off">
             <div class="form-group">
-                <input type="email" name="email" id="email" placeholder="Email Address" required/>
+                <input type="email" name="email" id="email" placeholder="Email Address" onChange={(event)=>setPassword(event.target.value)} />
             </div>
             <div class="form-group">
-                <input type="password" name="pwd" id="pwd" placeholder="Password" required/>
+                <input type="password" name="pwd" id="pwd" placeholder="Password" onChange={(event)=>setPassword(event.target.value)} />
             </div>
             <div class="form-group">
-                <button type="submit">Sign In</button>
+                <button className='btn'   onClick={getData}>Sign In</button>
             </div>
-            <span><p>Don't have an account? <Link to="/registration" >Registration here</Link></p>
+            <span><p>Don't have an account? <Link to="/registration" >Register here</Link></p>
         </span>
         </form>
            </div> 
            
         );
      }
+    }
 export default Singin;
